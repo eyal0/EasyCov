b36da3290d33b69385d99b619dcc9c3e1a348d64
eyal0 <109809+eyal0@users.noreply.github.com>
2019-12-12 09:10:02 -0500

    WIP

--- setup.py
+++ setup.py
@@ -1,10 +1,15 @@
-from setuptools import setup
+#!/usr/bin/env python2
+"""Setup and install EasyCov."""
+
+from setuptools import setup, find_packages
+import setuptools
 
 setup(
     name='EasyCov',
     version='1.0',
-    packages=['easycov',],
+    packages=find_packages(),
     license='MIT',
     long_description=open('README.md').read(),
     install_requires=['lcovparse'],
+    scripts=["bin/easycov"],
 )
