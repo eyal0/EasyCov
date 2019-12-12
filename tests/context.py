cb623eb73e638a65052cb26538ac11f888237398
eyal0 <109809+eyal0@users.noreply.github.com>
2019-12-11 23:35:34 -0500

    pylint easycov tests

--- tests/context.py
+++ tests/context.py
@@ -5,4 +5,4 @@ import os
 import sys
 sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)) + "../easycov"))
 
-import easycov.coverage
+import easycov.coverage # pylint: disable=wrong-import-position,unused-import
