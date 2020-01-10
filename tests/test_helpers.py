8697575cff4a65907858fd0a05df3e050fac0dcb
eyal0 <109809+eyal0@users.noreply.github.com>
2020-01-10 15:13:45 -0500

    Use tox to check py37 compatibility

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -8,11 +8,14 @@ import unittest
 
 class BaseTestCase(unittest.TestCase):
   """Base class for tests that need compare_lines."""
-  def compare_lines(self, actual_lines, expected_lines, filename):
+  def compare_lines(self, actual_lines, expected_lines, filename, rstrip=True):
     """Helper for comparing lines.
 
     Outputs a unified diff.
     """
+    if rstrip:
+      actual_lines = list(x.rstrip() for x in actual_lines)
+      expected_lines = list(x.rstrip() for x in expected_lines)
     self.assertEqual(actual_lines,
                      expected_lines,
                      "\n\n" + "\n".join(
